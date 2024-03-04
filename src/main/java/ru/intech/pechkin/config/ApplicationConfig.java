bb06deba7fc6c56845d4259743ab5adbee4df4bb
FrolovIV <FrolovIV@git.mail.com>
2024-03-04 20:07:43 +0300

    transactions for mongo are working now

--- src/main/java/ru/intech/pechkin/config/ApplicationConfig.java
+++ src/main/java/ru/intech/pechkin/config/ApplicationConfig.java
@@ -1,9 +1,24 @@
 package ru.intech.pechkin.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
+@EnableMongoRepositories(basePackages = {
+        "ru.intech.pechkin.messenger.infrastructure.persistance.repo",
+        "ru.intech.pechkin.corporate.infrastructure.persistance.repo"
+})
 public class ApplicationConfig {
+    @Bean
+    public PlatformTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
 }
